a38450d39cbca669b87e97a2cd7fd3094355de74
Alexander Zashev <azashev93@gmail.com>
2022-08-15 18:15:01 +0300

    ~

--- Exercise before the course/Lists as Stacks and Queues Lab/hot_potato_05.py
+++ Exercise before the course/Lists as Stacks and Queues Lab/hot_potato_05.py
@@ -8,3 +8,14 @@ while len(kids) > 1:
     print(f"Removed {kids.pop()}")
 
 print(f"Last is {kids.pop()}")
+
+
+# Hot Potato is a game in which children form a circle and toss a hot potato. The counting starts with the first kid.
+# Every nth toss, the child holding the potato leaves the game. When a kid leaves the game, it passes the potato to the
+# next kid. It continues until there is only one kid left.
+#
+# Create a program that simulates the game of Hot Potato.
+# On the first line, you will receive kids' names, separated by a single space.
+# On the second line, you will receive the nth toss (integer) in which a child leaves the game.
+# Print every kid who is removed from the circle in the format "Removed {kid}".
+# In the end, print the only kid left in the format "Last is {kid}".
