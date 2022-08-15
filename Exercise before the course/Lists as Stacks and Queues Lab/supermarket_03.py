f4bc7c2ff2cd16391c1a711093ff30a719ceba58
Alexander Zashev <azashev93@gmail.com>
2022-08-15 17:45:56 +0300

    ~

--- Exercise before the course/Lists as Stacks and Queues Lab/supermarket_03.py
+++ Exercise before the course/Lists as Stacks and Queues Lab/supermarket_03.py
@@ -14,3 +14,13 @@ while True:
         break
     else:
         queue.append(name)
+
+
+# Write a program that reads lines of input consisting of a customer's name and adds it to the end of a queue until
+# "End" is received.
+#
+# If, in the meantime, you receive the command "Paid", you should print each customer in the order they are served,
+# from the first to the last one) and empty the queue.
+#
+# When you receive "End", you should print the count of the remaining people in the queue in the format:
+# "{count} people remaining.".
