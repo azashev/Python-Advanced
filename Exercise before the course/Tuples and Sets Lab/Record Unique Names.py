e40fd548b940eb3bb3e9e28c5e6e1fb43fc760ba
Alexander Zashev <azashev93@gmail.com>
2022-08-25 22:01:10 +0300

    Added Record Unique Names

---
+++ Exercise before the course/Tuples and Sets Lab/Record Unique Names.py
@@ -0,0 +1,10 @@
+n = int(input())
+names = set()
+
+for _ in range(n):
+    names.add(input())
+
+print('\n'.join(names))
+
+# Write a program, which will take a list of names and print only the unique names in the list.
+# The order in which we print the result does not matter and the input will be valid.
