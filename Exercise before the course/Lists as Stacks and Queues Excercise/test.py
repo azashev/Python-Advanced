cbccff7b6d784784132060f430069ea591f85eb1
Alexander Zashev <azashev93@gmail.com>
2022-08-25 19:24:11 +0300

    ~

---
+++ Exercise before the course/Lists as Stacks and Queues Excercise/test.py
@@ -0,0 +1,13 @@
+# neshto = ["gosho", "pesho", "misho"]
+#
+# for x in neshto:
+#     for ch in x:
+#         print(ch)
+#
+# free_window = 3
+# current_free_window = free_window
+# current_free_window = 5
+# print(free_window)
+
+neshto = (3,)
+print(type(neshto))
