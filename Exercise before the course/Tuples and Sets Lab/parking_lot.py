8fd6aece0ed2aef5419d801a7032da49520bbddc
Alexander Zashev <azashev93@gmail.com>
2022-08-25 22:05:51 +0300

    ~

--- Exercise before the course/Tuples and Sets Lab/parking_lot.py
+++ Exercise before the course/Tuples and Sets Lab/parking_lot.py
@@ -14,3 +14,16 @@ if cars:
     print('\n'.join(cars))
 else:
     print("Parking Lot is Empty")
+
+# Write a program that:
+# • Records a car number for every car that enters the parking lot
+# • Removes a car number when the car leaves the parking lot
+#
+# On the first line, you will receive the number of commands - N.
+# On the following N lines, you will receive the direction and car's number in the format:
+# {direction}, {car_number}"
+#
+# The direction could only be "IN" or "OUT". Print the car numbers which are still in the parking lot.
+# Keep in mind that all car numbers must be unique.
+# If the parking lot is empty, print "Parking Lot is Empty".
+# Note: The order in which we print the result does not matter and the input will be valid.
