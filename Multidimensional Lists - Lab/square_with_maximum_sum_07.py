106e38e6e6f152416774a1f438c865b6e68873bb
Alexander Zashev <azashev93@gmail.com>
2022-09-21 17:28:50 +0300

    ~

--- Multidimensional Lists - Lab/square_with_maximum_sum_07.py
+++ Multidimensional Lists - Lab/square_with_maximum_sum_07.py
@@ -11,9 +11,9 @@ for row in range(rows - 1):
         current_sum = matrix[row][col] + matrix[row][col + 1] + matrix[row + 1][col] + matrix[row + 1][col + 1]
         if current_sum > sum:
             sum = current_sum
-            square = []
             first_list = [matrix[row][col], matrix[row][col + 1]]
             second_list = [matrix[row + 1][col], matrix[row + 1][col + 1]]
+            square.clear()
             square.append(first_list)
             square.append(second_list)
 
