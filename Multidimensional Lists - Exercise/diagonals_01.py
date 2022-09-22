189c6d0868d7fa5b5e82905de273dc11605b8dd8
Alexander Zashev <azashev93@gmail.com>
2022-09-22 14:17:40 +0300

    Added Diagonals

---
+++ Multidimensional Lists - Exercise/diagonals_01.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+matrix = [[int(x) for x in input().split(', ')] for x in range(n)]
+
+primary_diagonal, secondary_diagonal = [], []
+sum_primary_diagonal, sum_secondary_diagonal = 0, 0
+
+for row in range(n):
+    sum_primary_diagonal += matrix[row][row]
+    primary_diagonal.append(matrix[row][row])
+    sum_secondary_diagonal += (matrix[row][n - row - 1])
+    secondary_diagonal.append(matrix[row][n - row - 1])
+
+
+print(f"Primary diagonal: {', '.join(str(x) for x in primary_diagonal)}. Sum: {sum_primary_diagonal}")
+print(f"Secondary diagonal: {', '.join(str(x) for x in secondary_diagonal)}. Sum: {sum_secondary_diagonal}")
