a15798ab490fd9699d4afc61034f1e50a4bda27e
Alexander Zashev <azashev93@gmail.com>
2022-09-22 18:25:16 +0300

    ~

--- Multidimensional Lists - Exercise/snake_moves_07.py
+++ Multidimensional Lists - Exercise/snake_moves_07.py
@@ -1,3 +1,5 @@
+# First solution
+
 rows, cols = [int(x) for x in input().split()]
 
 snake = list(input())
@@ -18,3 +20,26 @@ for row in range(rows):
 
 matrix = [''.join(x) for x in matrix]
 print(*matrix, sep='\n')
+
+
+
+# Second solution
+#
+# rows, cols = [int(x) for x in input().split()]
+#
+# snake = list(input())
+#
+# matrix = []
+#
+# i = 0
+#
+# for row in range(rows):
+#     start, end, step = (0, cols, 1) if row % 2 == 0 else (cols - 1, -1, -1)
+#     word = [None] * cols
+#     for col in range(start, end, step):
+#         word[col] = snake[i % len(snake)]
+#         i += 1
+#     matrix.append(word)
+#
+# matrix = [''.join(x) for x in matrix]
+# print(*matrix, sep='\n')
