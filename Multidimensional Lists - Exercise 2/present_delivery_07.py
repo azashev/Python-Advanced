38776c36670d31ee6b5ca870e64f761e1ce5dbe3
Alexander Zashev <azashev93@gmail.com>
2022-09-25 19:37:48 +0300

    ~

--- Multidimensional Lists - Exercise 2/present_delivery_07.py
+++ Multidimensional Lists - Exercise 2/present_delivery_07.py
@@ -71,3 +71,53 @@ if nice_kids_presents_count == total_nice_kids:
     print(f"Good job, Santa! {nice_kids_presents_count} happy nice kid/s.")
 else:
     print(f"No presents for {total_nice_kids - nice_kids_presents_count} nice kid/s.")
+
+
+# The presents are ready, and Santa has to deliver them to the kids.
+#
+# You will receive an integer m for the number of presents Santa has and an integer n for the size of the neighborhood
+# with a square shape. On the following lines, you will receive the matrix, which represents the neighborhood.
+# Santa will be in a random cell, marked with the letter "S". Each cell stands for a house where children may live.
+# If the cell has "X" on it, that means there lives a naughty kid. Otherwise, if a nice kid lives there, the cell is
+# marked by "V".
+# There can also be cells marked with "C" for cookies. All of the empty positions will be marked with "-".
+#
+# Santa can move "up", "down", "left", "right" with one position each time. These will be the commands that you receive.
+#
+#
+# If he moves to a house with a nice kid, the kid receives a present, but if Santa reaches a house with a naughty kid,
+# he doesn't drop a present.
+# If the command sends Santa to a cell marked with "C", Santa eats cookies and becomes happy and extra generous to all
+# the kids around him* (meaning all of them will receive presents - it doesn't matter if naughty or nice).
+# If Santa has been to a house, the cell becomes "-".
+#
+# Note: *around him means on his left, right, upwards, and downwards by one cell.
+# In this case, Santa doesn't move to these cells, or if he does, he returns to the cell where the cookie was.
+#
+#
+# If Santa runs out of presents or receives the command "Christmas morning", you should end the program.
+# Keep in mind that you should check whether all the nice kids received presents.
+#
+#
+# Input:
+# • On the first line, you are given the integer m - the count of presents
+# • On the second - integer n - the size of the neighborhood
+# • The following n lines hold the values for every row
+# • On each of the following lines you will get a command
+#
+# Output:
+# • On the first line:
+#   - If Santa runs out of presents, but there are still some nice kids left print: "Santa ran out of presents!"
+# • Next, print the matrix.
+# • In the end, print one of these messages:
+#   - If he manages to give all the nice kids presents, print:
+#       "Good job, Santa! {count_nice_kids} happy nice kid/s."
+#   - Otherwise, print:
+#       "No presents for {count nice kids} nice kid/s."
+#
+#
+# Constraints:
+# • The size of the square matrix will be between [2…10].
+# • Santa's position will be marked with 'S'.
+# • There will always be at least 1 nice kid.
+# • There won't be a case where the cookie is on the border of the matrix.
--- Multidimensional Lists - Exercise 2/range_day_06.py
+++ Multidimensional Lists - Exercise 2/range_day_06.py
@@ -68,3 +68,55 @@ if total_targets > shot_targets:
 else:
     print(f"Training completed! All {total_targets} targets hit.")
 print(*indexes_of_shot_targets, sep='\n')
+
+
+# You are participating in a Firearm course. It is a training day at the shooting range.
+#
+# You will be given a matrix with 5 rows and 5 columns. It is a shotgun range represented as some symbols separated by a
+# single space:
+# • Your position is marked with the symbol "A"
+# • Targets marked with symbol "x"
+# • All of the empty positions will be marked with "."
+#
+# After the field state, you will be given an integer representing the number of commands you will receive.
+#
+# The possible commands are:
+# • "move {right/left/up/down} {steps}" – you should move in the given direction with the given steps. You can only move
+# if the field you want to step on is marked with ".".
+#
+# • "shoot {right/left/up/down}" – you should shoot in the given direction (from your current position without moving).
+# Beware that there might be targets that stand in the way of other targets, and you cannot reach them - you can shoot
+# only the nearest target. When you have shot a target, the field becomes empty position (".").
+#
+# Validate the positions since they can be outside the field.
+#
+#
+# Keep track of all the shot targets:
+# • If at any point there are no targets left, end the program and print:
+# "Training completed! All {count_targets} targets hit."
+#
+# • If, after you perform all the commands, there are some targets left print:
+# "Training not completed! {count_left_targets} targets left."
+#
+# Finally, print the index positions of the targets that you hit, as shown in the examples.
+#
+#
+# Input:
+# • 5 lines representing the field (symbols, separated by a single space)
+# • N - count of commands
+# • On the following N lines - the commands in the format described above
+#
+# Output:
+# • On the first line, print one of the following:
+#   - If all the targets were shot
+#       "Training completed! All {count_targets} targets hit."
+#
+#   - Otherwise:
+#       "Training not completed! {count_left_targets} targets left."
+#
+# • Finally, print the index positions "[{row}, {column}]" of the targets that you hit.
+#
+#
+# Constrains:
+# • All the commands will be valid
+# • There will always be at least one target
