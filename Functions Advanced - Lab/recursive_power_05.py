9f8c2e476ede2cee1544dcc50aa10722b986746a
Alexander Zashev <azashev93@gmail.com>
2022-09-27 22:06:33 +0300

    Update recursive_power_05.py

--- Functions Advanced - Lab/recursive_power_05.py
+++ Functions Advanced - Lab/recursive_power_05.py
@@ -6,4 +6,16 @@ def recursive_power(num, power):
     result = num * recursive_power(num, power - 1)
     return result
 
+
+# Create a recursive function called recursive_power() which should receive a number and a power.
+# Using recursion, return the result of number ** power.
+#
+#
+# Test code:
+print(recursive_power(2, 10))
 print(recursive_power(10, 100))
+#
+#
+# Output:
+# 1024
+# 10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
