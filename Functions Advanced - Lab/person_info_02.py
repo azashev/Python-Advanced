f555141b48a4d5e980d24f6d01dbead6f9676002
Alexander Zashev <azashev93@gmail.com>
2022-09-27 17:24:45 +0300

    Added Person Info

---
+++ Functions Advanced - Lab/person_info_02.py
@@ -0,0 +1,10 @@
+def get_info(name, age, town):
+    return f"This is {name} from {town} and he is {age} years old"
+
+person = {
+"name": "George",
+    "town": "Sofia",
+    "age": 20
+}
+
+print(get_info(**person))
