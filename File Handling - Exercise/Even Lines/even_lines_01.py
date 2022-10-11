8ec5a7fc7af7cfb8bd19a9446e028d5b68b31437
Alexander Zashev <azashev93@gmail.com>
2022-10-11 14:43:59 +0300

    Added Even Lines and Line Numbers

---
+++ File Handling - Exercise/Even Lines/even_lines_01.py
@@ -0,0 +1,14 @@
+symbols = ["-", ",", ", ", ".", "!", "?"]
+
+with open("text.txt", "r") as even_lines_file:
+    text = even_lines_file.readlines()
+
+for row in range(0, len(text), 2):
+    for symbol in symbols:
+        text[row] = text[row].replace(symbol, "@")
+
+    print(*text[row].split()[::-1], sep=" ")
+
+
+# Write a program that reads a text file and prints on the console its even lines. Line numbers start from 0.
+# Before you print the result, replace {"-", ",", ".", "!", "?"} with "@" and reverse the order of the words.
