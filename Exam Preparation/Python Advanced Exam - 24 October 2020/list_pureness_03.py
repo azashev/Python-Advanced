ccd994d4dc67ed964e2fa514508b6a19a4040141
Alexander Zashev <azashev93@gmail.com>
2022-10-14 13:37:24 +0300

    Added List Pureness

--- Exam Preparation/Python Advanced Exam - 24 October 2020/list_pureness_03.py
+++ Exam Preparation/Python Advanced Exam - 24 October 2020/list_pureness_03.py
@@ -1,5 +1,22 @@
-def best_list_pureness(*args, k=None):
-    return args[0]
+from collections import deque
+
+def best_list_pureness(*args):
+    numbers = deque(args[0])
+    k = args[1]
+    best_pureness = -1
+    rotation = 0
+    rotations_count = 0
+
+    for i in range(k):
+        current_pureness = sum(x*y for x, y in enumerate(numbers))
+        if current_pureness > best_pureness:
+            best_pureness = current_pureness
+            rotation = rotations_count
+        numbers.rotate()
+        rotations_count += 1
+
+    return f"Best pureness {best_pureness} after {rotation} rotations"
+
 
 test = ([4, 3, 2, 6], 4)
 result = best_list_pureness(*test)
